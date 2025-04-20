50196f3af170116ffa73422a9e4251a730fb5760
Dawid Paluch <dawid1paluch@gmail.com>
2025-04-20 16:41:39 +0100

    seperated bubbles animation into its own component

---
+++ src/bubblesBackground.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import styles from './css/bubblesBackground.module.css';
+
+import Footer from './footer.js';
+
+const BubblesBackground = ({animate}) => {
+
+  return (
+  <div className={styles.bubbles + " " + (animate ? styles.fadeoutBubbles: "")}>
+    <span style={{ "--i":12}}></span>
+    <span style={{ "--i":7}}></span>
+    <span style={{ "--i":21}}></span>
+    <span style={{"--i":30}}></span>
+    <span style={{"--i":12}}></span>
+    <span style={{"--i":16}}></span>
+    <span style={{"--i":9}}></span>
+    <span style={{"--i":13}}></span>
+    <span style={{"--i":21}}></span>
+    <span style={{"--i":32}}></span>
+    <span style={{"--i":19}}></span>
+    <span style={{"--i":15}}></span>
+    <span style={{"--i":17}}></span>
+    <span style={{"--i":23}}></span>
+    <span style={{"--i":16}}></span>
+    <span style={{"--i":27}}></span>
+    <span style={{"--i":12}}></span>
+    <span style={{"--i":28}}></span>
+    <span style={{"--i":31}}></span>
+    <span style={{"--i":29}}></span>
+    <span style={{"--i":33}}></span>
+    <span style={{"--i":13}}></span>
+    <span style={{"--i":37}}></span>
+    <span style={{"--i":11}}></span>
+    <span style={{"--i":7}}></span>
+  </div>
+  );
+}
+  
+  export default BubblesBackground;
--- src/mainMenu.js
+++ src/mainMenu.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import styles from './css/mainMenu.module.css';
 
+import BubblesBackground from './bubblesBackground.js';
 import Footer from './footer.js';
 
 const MainMenu = () => {
@@ -18,33 +19,7 @@ const MainMenu = () => {
 
   return (
     <div className={styles.background}>
-      <div className={styles.bubbles + " " + (animate ? styles.fadeoutBubbles: "")}>
-        <span style={{ "--i":12}}></span>
-        <span style={{ "--i":7}}></span>
-        <span style={{ "--i":21}}></span>
-        <span style={{"--i":30}}></span>
-        <span style={{"--i":12}}></span>
-        <span style={{"--i":16}}></span>
-        <span style={{"--i":9}}></span>
-        <span style={{"--i":13}}></span>
-        <span style={{"--i":21}}></span>
-        <span style={{"--i":32}}></span>
-        <span style={{"--i":19}}></span>
-        <span style={{"--i":15}}></span>
-        <span style={{"--i":17}}></span>
-        <span style={{"--i":23}}></span>
-        <span style={{"--i":16}}></span>
-        <span style={{"--i":27}}></span>
-        <span style={{"--i":12}}></span>
-        <span style={{"--i":28}}></span>
-        <span style={{"--i":31}}></span>
-        <span style={{"--i":29}}></span>
-        <span style={{"--i":33}}></span>
-        <span style={{"--i":13}}></span>
-        <span style={{"--i":37}}></span>
-        <span style={{"--i":11}}></span>
-        <span style={{"--i":7}}></span>
-      </div>
+      <BubblesBackground animate={animate}/>
       <div className={styles.mainMenuContainer + " " + (animate ? styles.fadeoutContainer: "")}>
         <h1>Welcome to PaceMaths</h1>
         <p className={styles.text}>This is the main menu.
