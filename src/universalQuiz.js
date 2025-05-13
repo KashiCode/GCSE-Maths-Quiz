0e1dee262671c840033da526351127344bace42e
Dawid Paluch <dawid1paluch@gmail.com>
2025-05-13 21:13:44 +0100

    universal quiz updated for the new questions

--- src/universalQuiz.js
+++ src/universalQuiz.js
@@ -8,15 +8,18 @@ import {
     ratioQuestionStyle3,
     ratioQuestionStyle4
 }from './jsQuestionLogic/ratioQuizQuestions.js';
+import {
+    factorisationQuestionStyle1
+}from './jsQuestionLogic/factorisationQuizQuestions.js';
 
 const UniversalQuiz = () => {
     //Returning the generic quiz component with the appropriate props for the ratio quiz
     return (
         <GenericQuiz
             quizTitle="Universal Quiz"
-            questionFunction = {[probabilityQuestionStyle1[0], ratioQuestionStyle1[0], ratioQuestionStyle2[0], ratioQuestionStyle3[0], ratioQuestionStyle4[0]]}
-            answerSection={[probabilityQuestionStyle1[1], ratioQuestionStyle1[1], ratioQuestionStyle2[1], ratioQuestionStyle3[1], ratioQuestionStyle4[1]]}
-            checkAnswerFunction={[probabilityQuestionStyle1[2], ratioQuestionStyle1[2], ratioQuestionStyle2[2], ratioQuestionStyle3[2], ratioQuestionStyle4[2]]}
+            questionFunction = {[probabilityQuestionStyle1[0], ratioQuestionStyle1[0], ratioQuestionStyle2[0], ratioQuestionStyle3[0], ratioQuestionStyle4[0], factorisationQuestionStyle1[0]]}
+            answerSection={[probabilityQuestionStyle1[1], ratioQuestionStyle1[1], ratioQuestionStyle2[1], ratioQuestionStyle3[1], ratioQuestionStyle4[1], factorisationQuestionStyle1[1]]}
+            checkAnswerFunction={[probabilityQuestionStyle1[2], ratioQuestionStyle1[2], ratioQuestionStyle2[2], ratioQuestionStyle3[2], ratioQuestionStyle4[2], factorisationQuestionStyle1[2]]}
         />
     )
 }
