6273e6e1d6739a693d67e4f11d73161da253d95e
Dawid Paluch <dawid1paluch@gmail.com>
2025-05-20 13:16:23 +0100

    new ratio questions added to the quizzes

--- src/ratioQuiz.js
+++ src/ratioQuiz.js
@@ -5,7 +5,10 @@ import {
     ratioQuestionStyle1,
     ratioQuestionStyle2,
     ratioQuestionStyle3,
-    ratioQuestionStyle4
+    ratioQuestionStyle4,
+    ratioQuestionStyle5,
+    ratioQuestionStyle6,
+    ratioQuestionStyle7
 }from './jsQuestionLogic/ratioQuizQuestions.js';
 
 const RatioQuiz = () => {
@@ -13,9 +16,9 @@ const RatioQuiz = () => {
     return (
         <GenericQuiz
             quizTitle="Ratio Quiz"
-            questionFunction = {[ratioQuestionStyle1[0], ratioQuestionStyle2[0], ratioQuestionStyle3[0], ratioQuestionStyle4[0]]}
-            answerSection={[ratioQuestionStyle1[1], ratioQuestionStyle2[1], ratioQuestionStyle3[1], ratioQuestionStyle4[1]]}
-            checkAnswerFunction={[ratioQuestionStyle1[2], ratioQuestionStyle2[2], ratioQuestionStyle3[2], ratioQuestionStyle4[2]]}
+            questionFunction = {[ratioQuestionStyle1[0], ratioQuestionStyle2[0], ratioQuestionStyle3[0], ratioQuestionStyle4[0], ratioQuestionStyle5[0], ratioQuestionStyle6[0], ratioQuestionStyle7[0]]}
+            answerSection={[ratioQuestionStyle1[1], ratioQuestionStyle2[1], ratioQuestionStyle3[1], ratioQuestionStyle4[1], ratioQuestionStyle5[1], ratioQuestionStyle6[1], ratioQuestionStyle7[1]]}
+            checkAnswerFunction={[ratioQuestionStyle1[2], ratioQuestionStyle2[2], ratioQuestionStyle3[2], ratioQuestionStyle4[2], ratioQuestionStyle5[2], ratioQuestionStyle6[2], ratioQuestionStyle7[2]]}
         />
     )
 }
--- src/universalQuiz.js
+++ src/universalQuiz.js
@@ -6,7 +6,10 @@ import {
     ratioQuestionStyle1,
     ratioQuestionStyle2,
     ratioQuestionStyle3,
-    ratioQuestionStyle4
+    ratioQuestionStyle4,
+    ratioQuestionStyle5,
+    ratioQuestionStyle6,
+    ratioQuestionStyle7
 }from './jsQuestionLogic/ratioQuizQuestions.js';
 import {
     factorisationQuestionStyle1
@@ -17,9 +20,9 @@ const UniversalQuiz = () => {
     return (
         <GenericQuiz
             quizTitle="Universal Quiz"
-            questionFunction = {[probabilityQuestionStyle1[0], ratioQuestionStyle1[0], ratioQuestionStyle2[0], ratioQuestionStyle3[0], ratioQuestionStyle4[0], factorisationQuestionStyle1[0]]}
-            answerSection={[probabilityQuestionStyle1[1], ratioQuestionStyle1[1], ratioQuestionStyle2[1], ratioQuestionStyle3[1], ratioQuestionStyle4[1], factorisationQuestionStyle1[1]]}
-            checkAnswerFunction={[probabilityQuestionStyle1[2], ratioQuestionStyle1[2], ratioQuestionStyle2[2], ratioQuestionStyle3[2], ratioQuestionStyle4[2], factorisationQuestionStyle1[2]]}
+            questionFunction = {[probabilityQuestionStyle1[0], ratioQuestionStyle1[0], ratioQuestionStyle2[0], ratioQuestionStyle3[0], ratioQuestionStyle4[0], ratioQuestionStyle5[0], ratioQuestionStyle6[0], ratioQuestionStyle7[0], factorisationQuestionStyle1[0]]}
+            answerSection={[probabilityQuestionStyle1[1], ratioQuestionStyle1[1], ratioQuestionStyle2[1], ratioQuestionStyle3[1], ratioQuestionStyle4[1], ratioQuestionStyle5[1], ratioQuestionStyle6[1], ratioQuestionStyle7[1], factorisationQuestionStyle1[1]]}
+            checkAnswerFunction={[probabilityQuestionStyle1[2], ratioQuestionStyle1[2], ratioQuestionStyle2[2], ratioQuestionStyle3[2], ratioQuestionStyle4[2], ratioQuestionStyle5[2], ratioQuestionStyle6[2], ratioQuestionStyle7[2], factorisationQuestionStyle1[2]]}
         />
     )
 }
