6eb5d18e175a849dddc51410d878ffbbeb79079a
Dawid Paluch <dawid1paluch@gmail.com>
2025-05-20 18:38:11 +0100

    new factorisation questions added to the quizzes

--- src/factorisationQuiz.js
+++ src/factorisationQuiz.js
@@ -3,7 +3,9 @@ import React, { use } from 'react';
 import GenericQuiz from './genericQuizInterface';
 import {
     factorisationQuestionStyle1,
-    factorisationQuestionStyle2
+    factorisationQuestionStyle2,
+    factorisationQuestionStyle3,
+    factorisationQuestionStyle4
 }from './jsQuestionLogic/factorisationQuizQuestions.js';
 
 const FactorisationQuiz = () => {
@@ -11,9 +13,9 @@ const FactorisationQuiz = () => {
     return (
         <GenericQuiz
             quizTitle="Ratio Quiz"
-            questionFunction = {[factorisationQuestionStyle1[0], factorisationQuestionStyle2[0]]}
-            answerSection={[factorisationQuestionStyle1[1], factorisationQuestionStyle2[1]]}
-            checkAnswerFunction={[factorisationQuestionStyle1[2], factorisationQuestionStyle2[2]]}
+            questionFunction = {[factorisationQuestionStyle1[0], factorisationQuestionStyle2[0], factorisationQuestionStyle3[0], factorisationQuestionStyle4[0]]}
+            answerSection={[factorisationQuestionStyle1[1], factorisationQuestionStyle2[1], factorisationQuestionStyle3[1], factorisationQuestionStyle4[1]]}
+            checkAnswerFunction={[factorisationQuestionStyle1[2], factorisationQuestionStyle2[2], factorisationQuestionStyle3[2], factorisationQuestionStyle4[2]]}
         />
     )
 }
--- src/universalQuiz.js
+++ src/universalQuiz.js
@@ -12,7 +12,10 @@ import {
     ratioQuestionStyle7
 }from './jsQuestionLogic/ratioQuizQuestions.js';
 import {
-    factorisationQuestionStyle1
+    factorisationQuestionStyle1,
+    factorisationQuestionStyle2,
+    factorisationQuestionStyle3,
+    factorisationQuestionStyle4
 }from './jsQuestionLogic/factorisationQuizQuestions.js';
 
 const UniversalQuiz = () => {
@@ -20,9 +23,9 @@ const UniversalQuiz = () => {
     return (
         <GenericQuiz
             quizTitle="Universal Quiz"
-            questionFunction = {[probabilityQuestionStyle1[0], ratioQuestionStyle1[0], ratioQuestionStyle2[0], ratioQuestionStyle3[0], ratioQuestionStyle4[0], ratioQuestionStyle5[0], ratioQuestionStyle6[0], ratioQuestionStyle7[0], factorisationQuestionStyle1[0]]}
-            answerSection={[probabilityQuestionStyle1[1], ratioQuestionStyle1[1], ratioQuestionStyle2[1], ratioQuestionStyle3[1], ratioQuestionStyle4[1], ratioQuestionStyle5[1], ratioQuestionStyle6[1], ratioQuestionStyle7[1], factorisationQuestionStyle1[1]]}
-            checkAnswerFunction={[probabilityQuestionStyle1[2], ratioQuestionStyle1[2], ratioQuestionStyle2[2], ratioQuestionStyle3[2], ratioQuestionStyle4[2], ratioQuestionStyle5[2], ratioQuestionStyle6[2], ratioQuestionStyle7[2], factorisationQuestionStyle1[2]]}
+            questionFunction = {[probabilityQuestionStyle1[0], ratioQuestionStyle1[0], ratioQuestionStyle2[0], ratioQuestionStyle3[0], ratioQuestionStyle4[0], ratioQuestionStyle5[0], ratioQuestionStyle6[0], ratioQuestionStyle7[0], factorisationQuestionStyle1[0], factorisationQuestionStyle2[0], factorisationQuestionStyle3[0], factorisationQuestionStyle4[0]]}
+            answerSection={[probabilityQuestionStyle1[1], ratioQuestionStyle1[1], ratioQuestionStyle2[1], ratioQuestionStyle3[1], ratioQuestionStyle4[1], ratioQuestionStyle5[1], ratioQuestionStyle6[1], ratioQuestionStyle7[1], factorisationQuestionStyle1[1], factorisationQuestionStyle2[1], factorisationQuestionStyle3[1], factorisationQuestionStyle4[1]]}
+            checkAnswerFunction={[probabilityQuestionStyle1[2], ratioQuestionStyle1[2], ratioQuestionStyle2[2], ratioQuestionStyle3[2], ratioQuestionStyle4[2], ratioQuestionStyle5[2], ratioQuestionStyle6[2], ratioQuestionStyle7[2], factorisationQuestionStyle1[2], factorisationQuestionStyle2[2], factorisationQuestionStyle3[2], factorisationQuestionStyle4[2]]}
         />
     )
 }
