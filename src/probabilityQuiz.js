d428172379ef22b6dafd45ca0be9a0fa28c1edf8
Maks Ostrynski <ostrynskimaks@gmail.com>
2025-05-22 22:51:31 +0100

    Added Q5,Q8,Q9,Q10 logic

--- src/probabilityQuiz.js
+++ src/probabilityQuiz.js
@@ -3,7 +3,12 @@ import {
     probabilityQuestionStyle1,
     probabilityQuestionStyle2,
     probabilityQuestionStyle3,
-    probabilityQuestionStyle4
+    probabilityQuestionStyle4,
+    probabilityQuestionStyle5,
+    probabilityQuestionStyle6,
+    probabilityQuestionStyle8,
+    probabilityQuestionStyle9,
+    probabilityQuestionStyle10 
 }from './jsQuestionLogic/probabilityQuizQuestions.js';
 
 const ProbabilityQuiz = () => {
@@ -11,9 +16,9 @@ const ProbabilityQuiz = () => {
     return (
         <GenericQuiz
             quizTitle="Probability Quiz"
-            questionFunction = {[probabilityQuestionStyle1[0], probabilityQuestionStyle2[0], probabilityQuestionStyle3[0], probabilityQuestionStyle4[0]]}
-            answerSection={[probabilityQuestionStyle1[1], probabilityQuestionStyle2[1], probabilityQuestionStyle3[1], probabilityQuestionStyle4[1]]}
-            checkAnswerFunction={[probabilityQuestionStyle1[2], probabilityQuestionStyle2[2], probabilityQuestionStyle3[2], probabilityQuestionStyle4[2]]}
+            questionFunction = {[probabilityQuestionStyle1[0], probabilityQuestionStyle2[0], probabilityQuestionStyle3[0], probabilityQuestionStyle4[0],probabilityQuestionStyle6[0],probabilityQuestionStyle8[0],probabilityQuestionStyle9[0],probabilityQuestionStyle5[0],probabilityQuestionStyle10[0]]}
+            answerSection={[probabilityQuestionStyle1[1], probabilityQuestionStyle2[1], probabilityQuestionStyle3[1], probabilityQuestionStyle4[1],probabilityQuestionStyle6[1],probabilityQuestionStyle8[1],probabilityQuestionStyle9[1],probabilityQuestionStyle5[1],probabilityQuestionStyle10[1]]}
+            checkAnswerFunction={[probabilityQuestionStyle1[2], probabilityQuestionStyle2[2], probabilityQuestionStyle3[2], probabilityQuestionStyle4[2],probabilityQuestionStyle6[2],probabilityQuestionStyle8[2],probabilityQuestionStyle9[2],probabilityQuestionStyle5[2],probabilityQuestionStyle10[2]]}
         />
     )
 }
