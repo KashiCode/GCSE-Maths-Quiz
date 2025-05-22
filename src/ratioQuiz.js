36c9dc055cdb492223da1ddb79b5dd3461c69a13
Maks Ostrynski <ostrynskimaks@gmail.com>
2025-05-22 22:35:04 +0100

    Added Ninth question logic

--- src/ratioQuiz.js
+++ src/ratioQuiz.js
@@ -9,7 +9,8 @@ import {
     ratioQuestionStyle5,
     ratioQuestionStyle6,
     ratioQuestionStyle7,
-    ratioQuestionStyle8
+    ratioQuestionStyle8,
+    ratioQuestionStyle9
 }from './jsQuestionLogic/ratioQuizQuestions.js';
 
 const RatioQuiz = () => {
@@ -17,9 +18,9 @@ const RatioQuiz = () => {
     return (
         <GenericQuiz
             quizTitle="Ratio Quiz"
-            questionFunction = {[ratioQuestionStyle1[0], ratioQuestionStyle2[0], ratioQuestionStyle3[0], ratioQuestionStyle4[0], ratioQuestionStyle5[0], ratioQuestionStyle6[0], ratioQuestionStyle7[0],ratioQuestionStyle8[0]]}
-            answerSection={[ratioQuestionStyle1[1], ratioQuestionStyle2[1], ratioQuestionStyle3[1], ratioQuestionStyle4[1], ratioQuestionStyle5[1], ratioQuestionStyle6[1], ratioQuestionStyle7[1],ratioQuestionStyle8[1]]}
-            checkAnswerFunction={[ratioQuestionStyle1[2], ratioQuestionStyle2[2], ratioQuestionStyle3[2], ratioQuestionStyle4[2], ratioQuestionStyle5[2], ratioQuestionStyle6[2], ratioQuestionStyle7[2],ratioQuestionStyle8[2]]}
+            questionFunction = {[ratioQuestionStyle1[0], ratioQuestionStyle2[0], ratioQuestionStyle3[0], ratioQuestionStyle4[0], ratioQuestionStyle5[0], ratioQuestionStyle6[0], ratioQuestionStyle7[0],ratioQuestionStyle8[0],ratioQuestionStyle9[0]]}
+            answerSection={[ratioQuestionStyle1[1], ratioQuestionStyle2[1], ratioQuestionStyle3[1], ratioQuestionStyle4[1], ratioQuestionStyle5[1], ratioQuestionStyle6[1], ratioQuestionStyle7[1],ratioQuestionStyle8[1],ratioQuestionStyle9[1]]}
+            checkAnswerFunction={[ratioQuestionStyle1[2], ratioQuestionStyle2[2], ratioQuestionStyle3[2], ratioQuestionStyle4[2], ratioQuestionStyle5[2], ratioQuestionStyle6[2], ratioQuestionStyle7[2],ratioQuestionStyle8[2],ratioQuestionStyle9[2]]}
         />
     )
 }
