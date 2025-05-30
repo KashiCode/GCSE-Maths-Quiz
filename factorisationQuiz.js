4d7d2399916cb36838c7809ba03f591e26007cbd
Maks Ostrynski <ostrynskimaks@gmail.com>
2025-05-30 02:15:26 +0100

    Added 5th factorisation quiz question

---
+++ factorisationQuiz.js
@@ -0,0 +1,24 @@
+import React, { use } from 'react';
+
+import GenericQuiz from './genericQuizInterface';
+import {
+    factorisationQuestionStyle1,
+    factorisationQuestionStyle2,
+    factorisationQuestionStyle3,
+    factorisationQuestionStyle4,
+    factorisationQuestionStyle5
+}from './jsQuestionLogic/factorisationQuizQuestions.js';
+
+const FactorisationQuiz = () => {
+    //Returning the generic quiz component with the appropriate props for the ratio quiz
+    return (
+        <GenericQuiz
+            quizTitle="Ratio Quiz"
+            questionFunction = {[factorisationQuestionStyle1[0], factorisationQuestionStyle2[0], factorisationQuestionStyle3[0], factorisationQuestionStyle4[0], factorisationQuestionStyle5[0]]}
+            answerSection={[factorisationQuestionStyle1[1], factorisationQuestionStyle2[1], factorisationQuestionStyle3[1], factorisationQuestionStyle4[1], factorisationQuestionStyle5[1]]}
+            checkAnswerFunction={[factorisationQuestionStyle1[2], factorisationQuestionStyle2[2], factorisationQuestionStyle3[2], factorisationQuestionStyle4[2], factorisationQuestionStyle5[2]]}
+        />
+    )
+}
+
+export default FactorisationQuiz;
